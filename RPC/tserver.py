880f5b8148e3ae1ff92554d94347d96f0cefeb59
Yang-Bihong <yangbihong@pku.edu.cn>
2023-08-31 23:22:57 +0800

    update tserver

--- RPC/tserver.py
+++ RPC/tserver.py
@@ -18,7 +18,8 @@ import QuestionAnswerServer.BlockListService as BlockListService
 import handler as Handler
 
 __HOST = '127.0.0.1'
-__PORT = 9091
+__PORT1 = 3030
+__PORT2 = 9091
 
 if __name__ == '__main__':
     # Handlers
@@ -29,19 +30,21 @@ if __name__ == '__main__':
     question_answer_processor = QuestionAnswerServer.Processor(question_answer_handler)
     block_processor = BlockListService.Processor(block_handler)
     
-    # Multiplexed Processor
-    multiplexed_processor = TMultiplexedProcessor()
-    multiplexed_processor.registerProcessor("QuestionAnswerService", question_answer_processor)
-    multiplexed_processor.registerProcessor("BlockListService", block_processor)
-    
     # Server setup
-    transport = TSocket.TServerSocket(port=__PORT)
+    transport1 = TSocket.TServerSocket(port=__PORT1)
+    transport2 = TSocket.TServerSocket(port=__PORT2)
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
     
     # Use TThreadPoolServer for multi-threading
-    rpcServer = TServer.TThreadPoolServer(multiplexed_processor, transport, tfactory, pfactory)
-    rpcServer.setNumThreads(5)
+    rpcServer1 = TServer.TThreadPoolServer(question_answer_processor, transport1, tfactory, pfactory)
+    rpcServer1.setNumThreads(5)
     
-    print(f'Starting the rpc server at port: {__PORT}')
-    rpcServer.serve()
+    rpcServer2 = TServer.TThreadPoolServer(block_processor, transport2, tfactory, pfactory)
+    rpcServer2.setNumThreads(5)
+    
+    print(f'Starting the rpc server at port: {__PORT1} for QuestionAnswerService')
+    rpcServer1.serve()
+    
+    print(f'Starting the rpc server at port: {__PORT2} for BlockListService')
+    rpcServer2.serve()
