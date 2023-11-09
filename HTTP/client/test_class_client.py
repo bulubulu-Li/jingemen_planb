69c35c63a510debee33b935d5d6d1adb4c5377a5
BiHong-Yang <yangbihong@pku.edu.cn>
2023-11-09 13:24:20 +0800

    十堰部署debug

--- HTTP/client/test_class_client.py
+++ HTTP/client/test_class_client.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 import json,requests,time,random
-from multiprocessing import Pool
+from multiprocessing import Pool,set_start_method
 
 def run_client(query):
     response = requests.post("http://127.0.0.1:9090/a", json.dumps({"query": query}))
@@ -18,25 +18,25 @@ def cal_time_result(time_list):
     print("average: {}".format(sum(time_list) / len(time_list)))
     print("qps:{:.4f}".format(len(time_list) / sum(time_list)))
 
-def single_test(query_list, num, process_id = 0):
+def single_test(query_list, num, process_id = 0,isGenerate=0):
     # query_list:待请求query列表，num请求个数
     print("running process: process-{}".format(process_id))
     time_list = []
     for i in range(num):
         start_time = time.time()
         query = random.choice(query_list)
-        requests.post("http://127.0.0.1:3130/a", json.dumps({"question": query,"isGenerate":0}))
+        requests.post("http://localhost:9091/qabot/question-answer", json.dumps({"question": query,"isGenerate":isGenerate}))
         end_time = time.time()
         time_list.append(end_time-start_time)
     return time_list
 
 
-def batch_test(query_list, process_num, request_num):
+def batch_test(query_list, process_num, request_num,isGenerate=0):
     # query_list:待请求query列表，process_num进程个数，request_num请求个数(每个进程)
     pool = Pool(processes=process_num)
     process_result = []
     for i in range(process_num):
-        process_result.append(pool.apply_async(single_test, args=(query_list, request_num, str(i), )))
+        process_result.append(pool.apply_async(single_test, args=(query_list, request_num, str(i), isGenerate)))
         # processes.append(Process(target=single_test, args=(query_list, request_num, str(i), )))
     
     pool.close()
@@ -56,7 +56,17 @@ def batch_test(query_list, process_num, request_num):
 
 if __name__ == "__main__":
     time_list = [0]
+    set_start_method('spawn')
     # print(run_client("你好啊")) # 单元测试
-    time_list = single_test(["你好啊","今天天气怎么样"], 100) # 批量单进程测试
-    # time_list = batch_test(["你好啊","今天天气怎么样"], 4, 100) # 多进程压测
+    print("===================================== 查询模式 =====================================")
+    time_list = single_test(["如何提取住房公积金？","出租车计价标准","农民专业合作社年度报告内容"], 100) # 批量单进程测试
+    cal_time_result(time_list=time_list)
+    print("===================================== 生成模式 =====================================")
+    time_list = single_test(["如何提取住房公积金？","出租车计价标准","农民专业合作社年度报告内容"], 10,isGenerate=1) # 批量单进程测试
+    cal_time_result(time_list=time_list)
+    print("===================================== 多进程测试 查询模式 =====================================")
+    time_list = batch_test(["如何提取住房公积金？","出租车计价标准","农民专业合作社年度报告内容"], 4, 100) # 多进程压测
+    cal_time_result(time_list=time_list)
+    print("===================================== 多进程测试 生成模式 =====================================")
+    time_list = batch_test(["如何提取住房公积金？","出租车计价标准","农民专业合作社年度报告内容"], 4, 10,isGenerate=1) # 多进程压测
     cal_time_result(time_list=time_list)
--- SearchSystem/LargeLanguageModel/chain.py
+++ SearchSystem/LargeLanguageModel/chain.py
@@ -13,149 +13,147 @@ class Chain:
     def retrieve(self, question, docs:list[DataForm]):
         pass
 
-def ChainFactory(environment)->Chain:
-    if environment is not None:
+class ChainGov(Chain):
+    def __init__(self):
+        self.chain = None
+        self.tokenizer = None
+        self.prompt_template = """
+        你现在是一个经验丰富，十分严谨的12345热线工作人员，负责解答市民的各种问题。现在，你会得到一份 背景知识 ，里面包括了回答市民问题所需要相关的知识。
+        你必须礼貌，准确，严谨，无遗漏地根据这份知识来回答用户的问题，尽量使用这份知识的原文，不要遗漏原文的每一句话。按照给的资料的顺序，在使用到参考文献的地方标出参考文献角标，如[1]。
+        不要列出参考文献列表。
+        如果你不知道答案，只回答"未找到答案"，不要编造答案。如果你的答案不是来自 背景知识 ，只回答"未找到答案"，不要根据你已有的知识回答。
+        你必须使用中文回答。
+
+        背景知识: {context}
+
+        问题: {question}
+        中文答案:"""
+        # self.init()
+
+    def init(self):
         from transformers import AutoTokenizer, AutoModel
+        log.info('chain init!!!')
+        if self.chain is None:
+            log.info("chain is None,start loading")
+            self.tokenizer = AutoTokenizer.from_pretrained('/jingmen/chatglm-6b', trust_remote_code=True)
+            self.chain = AutoModel.from_pretrained('/jingmen/chatglm-6b', trust_remote_code=True).half().cuda()
+            self.chain = self.chain.eval()
+            log.info("loading finished")
 
-        class ChainGov(Chain):
-            def __init__(self):
-                self.chain = None
-                self.tokenizer = None
-                self.prompt_template = """
-                你现在是一个经验丰富，十分严谨的12345热线工作人员，负责解答市民的各种问题。现在，你会得到一份 背景知识 ，里面包括了回答市民问题所需要相关的知识。
-                你必须礼貌，准确，严谨，无遗漏地根据这份知识来回答用户的问题，尽量使用这份知识的原文，不要遗漏原文的每一句话。按照给的资料的顺序，在使用到参考文献的地方标出参考文献角标，如[1]。
-                不要列出参考文献列表。
-                如果你不知道答案，只回答"未找到答案"，不要编造答案。如果你的答案不是来自 背景知识 ，只回答"未找到答案"，不要根据你已有的知识回答。
-                你必须使用中文回答。
+    def retrieve(self, question, docs:list[DataForm]):
+        context = '\n'
+        for i, doc in enumerate(docs):
+            # context + docs + question length should be less than 1500
+            if len(self.prompt_template)+ len(context) + len(doc.title)+2+len(doc.page_content) + len(question) < 1500:
+                context += f'{doc.title}\n{doc.page_content}\n'
+            else:
+                docs = docs[:i]
+                break
 
-                背景知识: {context}
+        prompt = self.prompt_template.format(context=context, question=question)
+        response, history = self.chain.chat(self.tokenizer, prompt, history=[])
+        log.info(f'generateing question:{question},response:{response}')
+        # return response[0],docs
+        return response[0]
 
-                问题: {question}
-                中文答案:"""
-                # self.init()
+class ChainWeb(Chain):
+    def __init__(self):
+        from langchain.text_splitter import CharacterTextSplitter
 
-            def init(self):
-                log.info('chain init!!!')
-                if self.chain is None:
-                    log.info("chain is None,start loading")
-                    self.tokenizer = AutoTokenizer.from_pretrained('/jingmen/chatglm-6b', trust_remote_code=True)
-                    self.chain = AutoModel.from_pretrained('/jingmen/chatglm-6b', trust_remote_code=True).half().cuda()
-                    self.chain = self.chain.eval()
-                    log.info("loading finished")
+        self.prompt_template = """
+        你现在是一个经验丰富，十分严谨的12345热线工作人员，负责解答市民的各种问题。现在，你会得到一份 背景知识 ，里面包括了回答市民问题所需要相关的知识。
+        你必须礼貌，准确，严谨，无遗漏地根据这份知识来回答用户的问题，尽量使用这份知识的原文，不要遗漏原文的每一句话。按照给的资料的顺序，在使用到参考文献的地方标出参考文献角标，如[1]。
+        不要列出参考文献列表。
+        如果你不知道答案，只回答"未找到答案"，不要编造答案。如果你的答案不是来自 背景知识 ，只回答"未找到答案"，不要根据你已有的知识回答。
+        你必须使用中文回答。
 
-            def retrieve(self, question, docs:list[DataForm]):
-                context = '\n'
-                for i, doc in enumerate(docs):
-                    # context + docs + question length should be less than 1500
-                    if len(self.prompt_template)+ len(context) + len(doc.title)+2+len(doc.page_content) + len(question) < 1500:
-                        context += f'{doc.title}\n{doc.page_content}\n'
-                    else:
-                        docs = docs[:i]
-                        break
+        背景知识: {context}
 
-                prompt = self.prompt_template.format(context=context, question=question)
-                response, history = self.chain.chat(self.tokenizer, prompt, history=[])
-                return response[0],docs
-        return  ChainGov()
-    else :
+        问题: {question}
+        中文答案:"""
+
+        self.api_key = ""
+        self.embeddings = None
+        self.text_splitter = CharacterTextSplitter(separator="。", chunk_size=300, chunk_overlap=0)
+        self.docsearch = None
+        self.chain = None
+        self.persist_directory = tools.searchSystemPath('db')
+        self.init()
+
+    def init(self):
         from langchain.prompts import PromptTemplate
         from langchain.embeddings.openai import OpenAIEmbeddings
-        from langchain.embeddings.openai import OpenAIEmbeddings
-        from langchain.text_splitter import CharacterTextSplitter
         from langchain.chains import RetrievalQA
         # from langchain.llms import ChatOpenAI as OpenAI
         from langchain.chat_models import ChatOpenAI as OpenAI
+        self.api_key = self.get_api_key()
+        self.embeddings = OpenAIEmbeddings(openai_api_key=self.api_key)
+        self.embedding()
+        self.chain_type_kwargs = {"prompt": PromptTemplate(template=self.prompt_template, input_variables=["context", "question"])}
+        self.chain = RetrievalQA.from_chain_type(llm=OpenAI(model_name="gpt-3.5-turbo-16k-0613", max_tokens=500, temperature=0), chain_type="stuff", retriever=self.docsearch.as_retriever(search_kwargs={'k': 5}), chain_type_kwargs=self.chain_type_kwargs, verbose=True, return_source_documents=True)
+
+    def get_api_key(self):
+        if self.api_key.startswith("sk-"):
+            return self.api_key
+        else:
+            if os.getenv("OPENAI_API_KEY") is not None:
+                self.api_key = os.getenv("OPENAI_API_KEY")
+                return self.api_key
+            else:
+                log.info('请在openai官网注册账号，获取api_key填写至程序内或命令行参数中')
+                exit()
+
+    def embedding(self):
         from langchain.vectorstores import Chroma
-        from langchain.document_loaders import PyPDFLoader
-        from langchain.document_loaders import Docx2txtLoader
-        from rouge import Rouge
+        if tools.getConfig("new_embedding") == False:
+            self.docsearch = Chroma(persist_directory=self.persist_directory, embedding_function=self.embeddings)
+            return
 
-        import openai as BaseOpenAI
+        loader = self.chain_loader()
+        split_docs = loader.load()
+        if len(split_docs) > 0:
+            if self.docsearch is None:
+                self.docsearch = Chroma.from_documents(split_docs, self.embeddings, persist_directory=self.persist_directory)
+            else:
+                self.docsearch.add_documents(split_docs)
 
+        tools.setConfig("new_embedding", False)
+
+    def chain_loader(self):
         from langchain.document_loaders.base import BaseLoader
         from langchain.docstore.document import Document
         from SearchSystem.DataManager import  SqlDataManager
-        from langchain.prompts import PromptTemplate
 
-        class ChainWeb(Chain):
-            def __init__(self):
-                self.prompt_template = """
-                你现在是一个经验丰富，十分严谨的12345热线工作人员，负责解答市民的各种问题。现在，你会得到一份 背景知识 ，里面包括了回答市民问题所需要相关的知识。
-                你必须礼貌，准确，严谨，无遗漏地根据这份知识来回答用户的问题，尽量使用这份知识的原文，不要遗漏原文的每一句话。按照给的资料的顺序，在使用到参考文献的地方标出参考文献角标，如[1]。
-                不要列出参考文献列表。
-                如果你不知道答案，只回答"未找到答案"，不要编造答案。如果你的答案不是来自 背景知识 ，只回答"未找到答案"，不要根据你已有的知识回答。
-                你必须使用中文回答。
+        class ChainLoader(BaseLoader):
+            def __init__(self) -> None:
+                super().__init__()
 
-                背景知识: {context}
-
-                问题: {question}
-                中文答案:"""
-
-                self.api_key = ""
-                self.embeddings = None
-                self.text_splitter = CharacterTextSplitter(separator="。", chunk_size=300, chunk_overlap=0)
-                self.docsearch = None
-                self.chain = None
-                self.persist_directory = tools.searchSystemPath('db')
-                self.init()
-
-            def init(self):
-                self.api_key = self.get_api_key()
-                self.embeddings = OpenAIEmbeddings(openai_api_key=self.api_key)
-                self.embedding()
-                self.chain_type_kwargs = {"prompt": PromptTemplate(template=self.prompt_template, input_variables=["context", "question"])}
-                self.chain = RetrievalQA.from_chain_type(llm=OpenAI(model_name="gpt-3.5-turbo-16k-0613", max_tokens=500, temperature=0), chain_type="stuff", retriever=self.docsearch.as_retriever(search_kwargs={'k': 5}), chain_type_kwargs=self.chain_type_kwargs, verbose=True, return_source_documents=True)
-
-            def get_api_key(self):
-                if self.api_key.startswith("sk-"):
-                    return self.api_key
-                else:
-                    if os.getenv("OPENAI_API_KEY") is not None:
-                        self.api_key = os.getenv("OPENAI_API_KEY")
-                        return self.api_key
+            def load(self) -> list:
+                docs = []
+                datamanager = SqlDataManager()
+                for item in datamanager:
+                    meta = item.metadata
+                    meta["docId"] = item.docId
+                    meta["title"] = item.title
+                    for key in meta:
+                        if meta[key] is None:
+                            meta[key] = ""
+                    doc = Document(page_content=item.title + "\n\n" + item.page_content, metadata=meta)
+                    if len(doc.page_content) > 1000:
+                        log.info(f'{item.title} is too long, {doc}')
                     else:
-                        log.info('请在openai官网注册账号，获取api_key填写至程序内或命令行参数中')
-                        exit()
+                        docs.extend(self.text_splitter.split_documents([doc]))
+                return docs
 
-            def embedding(self):
-                if tools.getConfig("new_embedding") == False:
-                    self.docsearch = Chroma(persist_directory=self.persist_directory, embedding_function=self.embeddings)
-                    return
+        return ChainLoader()
 
-                loader = self.chain_loader()
-                split_docs = loader.load()
-                if len(split_docs) > 0:
-                    if self.docsearch is None:
-                        self.docsearch = Chroma.from_documents(split_docs, self.embeddings, persist_directory=self.persist_directory)
-                    else:
-                        self.docsearch.add_documents(split_docs)
+    def retrieve(self, question,docs:list[DataForm]=[]):
+        return self.chain({'query': question})["result"]
 
-                tools.setConfig("new_embedding", False)
 
-            def chain_loader(self):
-                class ChainLoader(BaseLoader):
-                    def __init__(self) -> None:
-                        super().__init__()
+def ChainFactory(environment)->Chain:
+    if environment is not None:
+        return  ChainGov()
+    else :
 
-                    def load(self) -> list:
-                        docs = []
-                        datamanager = SqlDataManager()
-                        for item in datamanager:
-                            meta = item.metadata
-                            meta["docId"] = item.docId
-                            meta["title"] = item.title
-                            for key in meta:
-                                if meta[key] is None:
-                                    meta[key] = ""
-                            doc = Document(page_content=item.title + "\n\n" + item.page_content, metadata=meta)
-                            if len(doc.page_content) > 1000:
-                                log.info(f'{item.title} is too long, {doc}')
-                            else:
-                                docs.extend(self.text_splitter.split_documents([doc]))
-                        return docs
-
-                return ChainLoader()
-
-            def retrieve(self, question,docs:list[DataForm]=[]):
-                return self.chain({'query': question})["result"]
         return ChainWeb()
